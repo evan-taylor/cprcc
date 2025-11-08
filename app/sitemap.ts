0684803cb708513610e653e33761153b6258e1ce
Evan Taylor <eltaylor1104@gmail.com>
2025-11-07 20:29:38 -0800

    fix: site-map page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,7 +35,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
-      url: `${baseUrl}/sitemap`,
+      url: `${baseUrl}/site-map`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
